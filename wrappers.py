1fb89322085836f4d32434b296876ca44f04407b
XuTiany1 <tiany12138@gmail.com>
2024-10-31 23:59:03 -0400

    migrated react to current open ai, got everything running.

--- wrappers.py
+++ wrappers.py
@@ -7,7 +7,7 @@ import string
 from collections import Counter
 
     
-DATA_DIR = "data"
+DATA_DIR = "src/tot/data"
 HOTPOTQA_SPLIT_FILE = {
   "train": "hotpot_train_v1.1_simplified.json",
   "dev": "hotpot_dev_v1_simplified.json",
@@ -80,7 +80,7 @@ def f1_score(prediction, ground_truth):
 class HotPotQAWrapper(gym.Wrapper):
   def __init__(self, env, split):
     super().__init__(env)
-    data_file = f"{DATA_DIR}/{HOTPOTQA_SPLIT_FILE[split]}"
+    data_file = f"{DATA_DIR}/hotpotQA/{HOTPOTQA_SPLIT_FILE[split]}"
     self.data = json.load(open(data_file))
     self.data = [(d['question'], d['answer']) for d in self.data]
     self.data_idx = 0
@@ -136,67 +136,67 @@ class HotPotQAWrapper(gym.Wrapper):
   def __len__(self):
     return len(self.data)
 
-class FeverWrapper(gym.Wrapper):
-  def __init__(self, env, split):
-    super().__init__(env)
-    
-    data_path = f"./data/{FEVER_SPLIT_FILE[split]}"
-    with open(data_path, "r") as json_file:
-      json_list = list(json_file)
-
-    data = []
-    for json_str in json_list:
-      json_str = json.loads(json_str)
-      label = json_str["label"]
-      claim = json_str["claim"]
-      data.append((claim, label))
-
-    self.data = data
-    self.data_idx = 0
-    self.split = split
-
-  def reset(self, seed=None, return_info=False, options=None, idx=None):
-    self.env.reset(seed=seed, return_info=return_info, options=options)
-    try:
-      self.env.step('')
-    except:
-      pass
-    self.env.reset(seed=seed, return_info=return_info, options=options)
-    self.data_idx = int(np.random.randint(len(self.data))) if idx is None else idx
-    observation = f"Claim: {self.data[self.data_idx][0]}"
-    info = self._get_info()
-    return (observation, info) if return_info else observation
-
-  def _get_info(self):
-    return {
-      "steps": self.steps, 
-      "answer": self.answer,
-      "question": self.data[self.data_idx][0], 
-      "fever_split": self.split
-    }
-
-  def get_reward(self, info):
-    if info['answer'] is not None:
-      label = normalize_answer(self.data[self.data_idx][1])
-      pred = normalize_answer(info['answer'])
-      if label == pred:
-        return 1
-    return 0
-
-  def step(self, action):
-    # TODO: first step obs does not have question. 
-    obs, _, done, info = self.env.step(action)
-    reward = self.get_reward(info)
-    if done:
-      obs = f"Episode finished, reward = {reward}\n"
-      info.update({"gt_answer": self.data[self.data_idx][1], "question_idx": self.data_idx})
-      info.update({'em': reward, 'reward': reward, 'f1': reward})
-    return obs, reward, done, info
-    
-  def __len__(self):
-    return len(self.data)
-  
-  
+#class FeverWrapper(gym.Wrapper):
+#  def __init__(self, env, split):
+#    super().__init__(env)
+#    
+#    data_path = f"./data/{FEVER_SPLIT_FILE[split]}"
+#    with open(data_path, "r") as json_file:
+#      json_list = list(json_file)
+#
+#    data = []
+#    for json_str in json_list:
+#      json_str = json.loads(json_str)
+#      label = json_str["label"]
+#      claim = json_str["claim"]
+#      data.append((claim, label))
+#
+#    self.data = data
+#    self.data_idx = 0
+#    self.split = split
+#
+#  def reset(self, seed=None, return_info=False, options=None, idx=None):
+#    self.env.reset(seed=seed, return_info=return_info, options=options)
+#    try:
+#      self.env.step('')
+#    except:
+#      pass
+#    self.env.reset(seed=seed, return_info=return_info, options=options)
+#    self.data_idx = int(np.random.randint(len(self.data))) if idx is None else idx
+#    observation = f"Claim: {self.data[self.data_idx][0]}"
+#    info = self._get_info()
+#    return (observation, info) if return_info else observation
+#
+#  def _get_info(self):
+#    return {
+#      "steps": self.steps, 
+#      "answer": self.answer,
+#      "question": self.data[self.data_idx][0], 
+#      "fever_split": self.split
+#    }
+#
+#  def get_reward(self, info):
+#    if info['answer'] is not None:
+#      label = normalize_answer(self.data[self.data_idx][1])
+#      pred = normalize_answer(info['answer'])
+#      if label == pred:
+#        return 1
+#    return 0
+#
+#  def step(self, action):
+#    # TODO: first step obs does not have question. 
+#    obs, _, done, info = self.env.step(action)
+#    reward = self.get_reward(info)
+#    if done:
+#      obs = f"Episode finished, reward = {reward}\n"
+#      info.update({"gt_answer": self.data[self.data_idx][1], "question_idx": self.data_idx})
+#      info.update({'em': reward, 'reward': reward, 'f1': reward})
+#    return obs, reward, done, info
+#    
+#  def __len__(self):
+#    return len(self.data)
+#  
+#  
 class LoggingWrapper(gym.Wrapper):
   def __init__(self, env, folder="trajs", file_id=None):
     super().__init__(env)
